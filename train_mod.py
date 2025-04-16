02850f387cdf4f5488838ffb645285ee0f6edad3
Polina Stepanenko <98809151+apollins@users.noreply.github.com>
2025-04-16 03:32:41 +0300

    Add types of residual displacement

---
+++ train_mod.py
@@ -0,0 +1,203 @@
+import argparse
+import os, sys
+import json
+import math
+import numpy as np
+import copy
+
+from config import config
+from model import siMLPe as Model
+from datasets.h36m import H36MDataset
+from utils.logger import get_logger, print_and_log_info
+from utils.pyt_utils import link_file, ensure_dir
+from datasets.h36m_eval import H36MEval
+
+from custom_test import test
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--exp-name', type=str, default=None, help='=exp name')
+parser.add_argument('--seed', type=int, default=888, help='=seed')
+parser.add_argument('--temporal-only', action='store_true', help='=temporal only')
+parser.add_argument('--layer-norm-axis', type=str, default='spatial', help='=layernorm axis')
+parser.add_argument('--with-normalization', action='store_true', help='=use layernorm')
+parser.add_argument('--spatial-fc', action='store_true', help='=use only spatial fc')
+parser.add_argument('--num', type=int, default=64, help='=num of blocks')
+parser.add_argument('--weight', type=float, default=1., help='=loss weight')
+parser.add_argument('--residual-type', type=str, default='ours', choices=['none', 'consecutive', 'before_idct', 'ours'], help='=type of residual displacement')
+args = parser.parse_args()
+
+torch.use_deterministic_algorithms(True)
+torch.manual_seed(args.seed)
+writer = SummaryWriter()
+acc_log = open(args.exp_name, 'a')
+
+config.motion_fc_in.temporal_fc = args.temporal_only
+config.motion_fc_out.temporal_fc = args.temporal_only
+config.motion_mlp.norm_axis = args.layer_norm_axis
+config.motion_mlp.spatial_fc_only = args.spatial_fc
+config.motion_mlp.with_normalization = args.with_normalization
+config.motion_mlp.num_layers = args.num
+config.motion_rnn.with_normalization = args.with_normalization
+
+# === RESIDUAL TYPE SELECTION ===
+if args.residual_type == 'none':
+    config.pre_dct = False
+    config.post_dct = False
+    config.residual_output = False
+elif args.residual_type == 'consecutive':
+    config.pre_dct = True
+    config.post_dct = True
+    config.residual_output = True
+elif args.residual_type == 'before_idct':
+    config.pre_dct = True
+    config.post_dct = False
+    config.residual_output = True
+elif args.residual_type == 'ours':
+    config.pre_dct = True
+    config.post_dct = True
+    config.residual_output = True
+
+acc_log.write(f"Seed: {args.seed}\nResidual Type: {args.residual_type}\n")
+
+# === DCT ===
+def get_dct_matrix(N):
+    dct_m = np.eye(N)
+    for k in np.arange(N):
+        for i in np.arange(N):
+            w = np.sqrt(2 / N)
+            if k == 0:
+                w = np.sqrt(1 / N)
+            dct_m[k, i] = w * np.cos(np.pi * (i + 1 / 2) * k / N)
+    idct_m = np.linalg.inv(dct_m)
+    return dct_m, idct_m
+
+dct_m, idct_m = get_dct_matrix(config.motion.h36m_input_length_dct)
+dct_m = torch.tensor(dct_m).float().cuda().unsqueeze(0)
+idct_m = torch.tensor(idct_m).float().cuda().unsqueeze(0)
+
+def update_lr_multistep(nb_iter, total_iter, max_lr, min_lr, optimizer):
+    if nb_iter > 30000:
+        current_lr = min_lr
+    else:
+        current_lr = max_lr
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = current_lr
+    return optimizer, current_lr
+
+def gen_velocity(m):
+    return m[:, 1:] - m[:, :-1]
+
+def train_step(h36m_motion_input, h36m_motion_target, model, optimizer, nb_iter, total_iter, max_lr, min_lr):
+    if config.pre_dct:
+        b, n, c = h36m_motion_input.shape
+        h36m_motion_input_ = torch.matmul(dct_m[:, :, :config.motion.h36m_input_length], h36m_motion_input.cuda())
+    else:
+        h36m_motion_input_ = h36m_motion_input.clone()
+
+    motion_pred = model(h36m_motion_input_.cuda())
+
+    if config.post_dct:
+        motion_pred = torch.matmul(idct_m[:, :config.motion.h36m_input_length, :], motion_pred)
+
+    if config.residual_output:
+        offset = h36m_motion_input[:, -1:].cuda()
+        motion_pred = motion_pred[:, :config.motion.h36m_target_length] + offset
+    else:
+        motion_pred = motion_pred[:, :config.motion.h36m_target_length]
+
+    b, n, c = h36m_motion_target.shape
+    motion_pred = motion_pred.reshape(b, n, 22, 3).reshape(-1, 3)
+    h36m_motion_target = h36m_motion_target.cuda().reshape(b, n, 22, 3).reshape(-1, 3)
+    loss = torch.mean(torch.norm(motion_pred - h36m_motion_target, 2, 1))
+
+    if config.use_relative_loss:
+        motion_pred = motion_pred.reshape(b, n, 22, 3)
+        dmotion_pred = gen_velocity(motion_pred)
+        motion_gt = h36m_motion_target.reshape(b, n, 22, 3)
+        dmotion_gt = gen_velocity(motion_gt)
+        dloss = torch.mean(torch.norm((dmotion_pred - dmotion_gt).reshape(-1, 3), 2, 1))
+        loss = loss + dloss
+    else:
+        loss = loss.mean()
+
+    writer.add_scalar('Loss/angle', loss.detach().cpu().numpy(), nb_iter)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    optimizer, current_lr = update_lr_multistep(nb_iter, total_iter, max_lr, min_lr, optimizer)
+    writer.add_scalar('LR/train', current_lr, nb_iter)
+
+    return loss.item(), optimizer, current_lr
+
+print("[DEBUG] Running config:")
+print("pre_dct =", config.pre_dct)
+print("post_dct =", config.post_dct)
+print("residual_output =", config.residual_output)
+
+model = Model(config)
+model.train().cuda()
+
+config.motion.h36m_target_length = config.motion.h36m_target_length_train
+dataset = H36MDataset(config, 'train', config.data_aug)
+
+dataloader = DataLoader(dataset, batch_size=config.batch_size, num_workers=config.num_workers,
+                        drop_last=True, shuffle=True, pin_memory=True)
+
+eval_config = copy.deepcopy(config)
+eval_config.motion.h36m_target_length = eval_config.motion.h36m_target_length_eval
+eval_dataset = H36MEval(eval_config, 'test')
+
+eval_dataloader = DataLoader(eval_dataset, batch_size=128, num_workers=1,
+                             drop_last=False, shuffle=False, pin_memory=True)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=config.cos_lr_max, weight_decay=config.weight_decay)
+
+ensure_dir(config.snapshot_dir)
+logger = get_logger(config.log_file, 'train')
+link_file(config.log_file, config.link_log_file)
+
+print_and_log_info(logger, json.dumps(config, indent=4, sort_keys=True))
+
+if config.model_pth:
+    state_dict = torch.load(config.model_pth)
+    model.load_state_dict(state_dict, strict=True)
+    print_and_log_info(logger, f"Loading model path from {config.model_pth}")
+
+# === TRAINING LOOP ===
+nb_iter = 0
+avg_loss = 0.
+avg_lr = 0.
+
+while (nb_iter + 1) < config.cos_lr_total_iters:
+    for h36m_motion_input, h36m_motion_target in dataloader:
+        loss, optimizer, current_lr = train_step(h36m_motion_input, h36m_motion_target, model, optimizer, nb_iter,
+                                                 config.cos_lr_total_iters, config.cos_lr_max, config.cos_lr_min)
+        avg_loss += loss
+        avg_lr += current_lr
+
+        if (nb_iter + 1) % config.print_every == 0:
+            avg_loss /= config.print_every
+            avg_lr /= config.print_every
+            print_and_log_info(logger, f"Iter {nb_iter + 1} Summary:")
+            print_and_log_info(logger, f"\t lr: {avg_lr:.6f} \t Training loss: {avg_loss:.4f}")
+            avg_loss = 0
+            avg_lr = 0
+
+        if (nb_iter + 1) % config.save_every == 0:
+            path = os.path.join(config.snapshot_dir, f'model-iter-{nb_iter + 1}.pth')
+            torch.save(model.state_dict(), path)
+            model.eval()
+            acc_tmp = test(eval_config, model, eval_dataloader)
+            print(f"Test Results @ {nb_iter + 1}: {acc_tmp}")
+            acc_log.write(f"{nb_iter + 1}\n{' '.join(str(a) for a in acc_tmp)}\n")
+            model.train()
+
+        if (nb_iter + 1) == config.cos_lr_total_iters:
+            break
+        nb_iter += 1
+
+writer.close()
